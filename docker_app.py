14385dcee8dd21dc5afb262d52e24e9f11534455
Aaron Edwards <aaron@cloudgenix.com>
2018-12-27 15:56:28 -0800

    Make responses pretty w/docker + ngnx by default

--- docker_app.py
+++ docker_app.py
@@ -11,9 +11,10 @@ from .topo import create_app
 auth_token = get_docker_secret('cgx_auth_token', default=None)
 memcached = get_docker_secret('cgx_memcached', default=None)
 debug = get_docker_secret('cgx_debug', default=False, cast_to=bool)
+always_pretty = get_docker_secret('cgx_always_pretty', default=True, cast_to=bool)
 
 # create an app instance
-app = create_app(auth_token=auth_token, memcached=memcached)
+app = create_app(auth_token=auth_token, memcached=memcached, always_pretty=always_pretty)
 
 if __name__ == '__main__':
     app.run(debug=debug)
